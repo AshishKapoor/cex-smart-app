21a91824f40dc5316379a8e0b01249d31743dc61
Ashish Kapoor <swiftobjc@gmail.com>
2017-06-23 11:02:59 +0530

    added ui structure and components

--- CEX - My Smart Price Tracker/CXTradeCurrencyVC.swift
+++ CEX - My Smart Price Tracker/CXTradeCurrencyVC.swift
@@ -1,5 +1,5 @@
 //
-//  CXTradeCurrencyTVC.swift
+//  CXTradeCurrencyVC.swift
 //  CEX - My Smart Price Tracker
 //
 //  Created by Ashish Kapoor on 23/06/17.
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class CXTradeCurrencyTVC: UITableViewController {
+class CXTradeCurrencyVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Trade History"
+        title                   = "Trade History"
+        
+        tableView.dataSource    = self
+        tableView.delegate      = self
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -28,26 +33,26 @@ class CXTradeCurrencyTVC: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        cell.textLabel?.text = "BTC"
+        cell.detailTextLabel?.text = "$5000"
+        
         return cell
     }
-    */
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -92,5 +97,4 @@ class CXTradeCurrencyTVC: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
